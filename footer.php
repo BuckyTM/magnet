5f2a08d66b78b545b8143fbe308f69385dc78373
BuckyTM <76104163+BuckyTM@users.noreply.github.com>
2023-03-17 20:40:50 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	<script type="text/javascript" src="//tajs.qq.com/stats?sId=66540845" charset="utf-8"></script>
+	<script type="text/javascript" src="" charset="utf-8"></script>
 	<div id="copyright" class="container">
 		<p class="muted credit">Copyright &copy; <a href="https://github.com/" target="_blank">messycode</a>, All Rights Reserved,Power by <a href="https://twitter.github.io/bootstrap/" target="_blank">bootstrap</a>,Design by <a href="https://9527.eu.org" target="_blank">buckytm</a></p>
 	</div>
