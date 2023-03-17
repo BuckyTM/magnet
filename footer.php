31dea33902480a6a7593fda13c91f8bc0409b190
BuckyTM <76104163+BuckyTM@users.noreply.github.com>
2023-03-17 20:43:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<script type="text/javascript" src="" charset="utf-8"></script>
 	<div id="copyright" class="container">
-		<p class="muted credit">Copyright &copy; <a href="https://github.com/" target="_blank">messycode</a>, All Rights Reserved,Power by <a href="https://twitter.github.io/bootstrap/" target="_blank">bootstrap</a>,Design by <a href="https://9527.eu.org" target="_blank">buckytm</a></p>
+		<p class="muted credit">Copyright &copy; <a href="https://github.com/" target="_blank">messycode</a>, All Rights Reserved,Power by <a href="https://twitter.github.io/bootstrap/" target="_blank">bootstrap</a>,Design by <a href="" target="_blank">buckytm</a></p>
 	</div>
 </div>
